5d4c849c2b82b351e151bed871cba95397ffa231
Florian Lüscher <florian@florianluescher.ch>
2012-11-12 13:59:26 +0100

    Add abstract attribute filter

---
+++ src/main/java/ch/fhnw/apsifilter/filter/AbstractAttributeFilter.java
@@ -0,0 +1,42 @@
+package ch.fhnw.apsifilter.filter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+
+import ch.fhnw.apsifilter.DomFilter;
+
+public abstract class AbstractAttributeFilter implements DomFilter {
+	private final Map<String, String[]> attributes;
+	
+	protected AbstractAttributeFilter() {
+		attributes = new HashMap<String, String[]>();
+	}
+	
+	protected void add(String elem, String... attributes) {
+		this.attributes.put(elem, attributes);
+	}
+	
+	public void filter(Node node) {
+		if(node instanceof Element ) {
+			final Element cur = (Element) node;
+			final List<String> toConsider = attributes.get(cur.tagName()) != null
+											? Arrays.asList(attributes.get(cur.tagName()))
+											: new ArrayList<String>(0);
+					
+			for(Attribute attr : cur.attributes()) {
+				if(toConsider.contains(attr.getKey()))
+					filterAttribute(attr);
+			}
+		}
+	}
+	
+	protected abstract void filterAttribute(Attribute attr);
+	
+}
