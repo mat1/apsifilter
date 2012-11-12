2024bd2c20d66896baa08c9df9a3e218c40ac6c6
Florian Lüscher <florian@florianluescher.ch>
2012-11-12 12:16:21 +0100

    Read file content

--- src/main/java/ch/fhnw/apsifilter/HtmlFilter.java
+++ src/main/java/ch/fhnw/apsifilter/HtmlFilter.java
@@ -1,5 +1,11 @@
 package ch.fhnw.apsifilter;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.jsoup.nodes.Document;
 
 public class HtmlFilter {
@@ -23,15 +29,38 @@ public class HtmlFilter {
 		pipe = Pipe.createPipe();
 	}
 	
+	
 	private String filter(String filename) {
-		String htmlText = readHtmlFromFile(filename);
-		
-		Document d = pipe.filter(htmlText);
-		
-		return d.html();
+		try {
+			String htmlText = readHtmlFromFile(filename);
+			Document d = pipe.filter(htmlText);
+			
+			return d.html();
+			
+		} catch (FileNotFoundException ex) {
+			System.err.println("Error accessing file: " + ex.getMessage());
+			return "";
+		}
 	}
 	
-	private String readHtmlFromFile(String filename) {
-		return "";
+	private String readHtmlFromFile(String filename) throws FileNotFoundException {
+		final File f = new File(filename);
+		if(!f.exists()) throw new FileNotFoundException("Unable to find file.");
+		if(!f.canRead()) throw new FileNotFoundException("Could not read file.");
+		
+		try{
+			final StringBuilder builder = new StringBuilder();
+			final BufferedReader in = new BufferedReader(new FileReader(f));
+			final char[] buf = new char[1024];
+			
+			int cnt;
+			while((cnt = in.read(buf)) != -1)
+				builder.append(buf, 0, cnt);
+			
+			in.close();
+			return builder.toString();
+		} catch(IOException ex) {
+			throw new FileNotFoundException("Error reading file: " + ex.getMessage());
+		}
 	}
 }
