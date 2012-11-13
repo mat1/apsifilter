6f269c8bd1626284ef22a1909d3cdb2be62a1504
Florian Lüscher <florian@florianluescher.ch>
2012-11-13 11:35:42 +0100

    Filter @import in css

--- src/main/java/ch/fhnw/apsifilter/filter/css/CssCleaner.java
+++ src/main/java/ch/fhnw/apsifilter/filter/css/CssCleaner.java
@@ -6,29 +6,34 @@ public final class CssCleaner {
 //	private static final String XBL_PATTERN = "-moz-binding:[\\s\\t]*url[\\s\\t]*\\([\\s\\t0-9a-zA-Z]*.xml(#[0-9a-zA-Z]*)?";
 //	private static final String HTC_PATTERN = "behavior:[\\s\\t]*url[\\s\\t]*\\([\\s\\t0-9a-zA-Z]*.htc";
 	
-	private static final String XBL_PATTERN = "-moz-binding(:[0-9a-zA-z\\s\\t\\(\\)\\#.\\\\/\\-\\+]*(;)?)?";
-	private static final String HTC_PATTERN = "behavior(:[0-9a-zA-z\\s\\t\\(\\)\\#.:\\\\/\\-\\+]*(;)?)?";
+	private static final String XBL_PATTERN = "-moz-binding([\\s\\t]*:[0-9a-zA-z\\s\\t\\(\\)\\#.\\\\/\\-\\+]*(;)?)?";
+	private static final String HTC_PATTERN = "behavior([\\s\\t]*:[0-9a-zA-z\\s\\t\\(\\)\\#.:\\\\/\\-\\+]*(;)?)?";
+	private static final String IMPORT_PATTERN = "@import([0-9a-zA-z\\s\\t\\(\\)\\#.:\\\\/\\-\\+]*(;)?)?";
 	
 	private final Pattern xblPattern;
 	private final Pattern htcPattern;
+	private final Pattern importPattern;
 	private final boolean strict;
 	
 	private CssCleaner(boolean strict) {
 		xblPattern = Pattern.compile(XBL_PATTERN);
 		htcPattern = Pattern.compile(HTC_PATTERN);
+		importPattern = Pattern.compile(IMPORT_PATTERN);
 		this.strict = strict;
 	}
 	
 	private boolean mayBeMalicious(final String css) {
 		return xblPattern.matcher(css).find() ||
-			   htcPattern.matcher(css).find();
+			   htcPattern.matcher(css).find() ||
+			   importPattern.matcher(css).find();
 	}
 	
 	public String getCleanedCss(final String css) {
 		if(mayBeMalicious(css)){
 			if(strict) return "";
 			else return css.replaceAll(XBL_PATTERN, "ignored: 0;")
-						   .replaceAll(HTC_PATTERN, "ignored: 0;");
+						   .replaceAll(HTC_PATTERN, "ignored: 0;")
+						   .replaceAll(IMPORT_PATTERN, "ignored: 0;");
 		}
 		
 		return css;
